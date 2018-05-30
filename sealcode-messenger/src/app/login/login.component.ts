8760d1e1f47d392b44ba36caba3665a1e3f45aeb
Michał Starski <32843198+michalStarski@users.noreply.github.com>
2018-05-30 08:38:34 +0200

    Update login.component.ts

--- sealcode-messenger/src/app/login/login.component.ts
+++ sealcode-messenger/src/app/login/login.component.ts
@@ -18,31 +18,6 @@ export class LoginComponent implements OnInit {
 
   private users: Object;
 
-  private subscriptions: Array<Subscription> = [];
-
-  //MOCK VALIDATION
-
-  async getData(event, body){
-    let promise = new Promise((resolve, reject) => {
-      this.subscriptions.push(
-        this.httpService.getData().subscribe(
-          data => { this.users = data; resolve();},
-          error => { alert('Oops!'); reject(); }
-        )
-      );
-    }).then(() => {
-      let username = body.username;
-      let password = body.password;
-      Object.keys(this.users).map(key => {
-        if(username === this.users[key].username && password === this.users[key].password) {
-          localStorage.setItem('isLogged', "someFancyHash");
-        }
-      });
-      if(!localStorage.getItem("isLogged"))
-        alert('Error!');
-    });
-  }
-
   ngOnInit() {
   }
 
