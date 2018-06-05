6a41fd41be9ecdee58af9a319c801d16918b3e3c
rom3k <romaszkin13@o2.pl>
2018-06-05 15:11:16 +0200

    minor index.js update

--- sealcode-messenger/index.js
+++ sealcode-messenger/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const mongoDb = require('mongodb').MongoClient;
+const db = require('./database.js');
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.post("/auth", (req, res, next) => {
+  db.getData(req.body);
 });
 
 
